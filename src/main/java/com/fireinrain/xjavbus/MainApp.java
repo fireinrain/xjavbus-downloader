9674178c15abc7ec8a1190e30fc1e4129bd3fae1
sunrise <575563079@qq.com>
2022-10-18 15:15:50 +0800

    init this repo

---
+++ src/main/java/com/fireinrain/xjavbus/MainApp.java
@@ -0,0 +1,16 @@
+package com.fireinrain.xjavbus;
+
+/**
+ * @Description: 采集javbus数据
+ * @Author : fireinrain
+ * @Site : https://github.com/fireinrain
+ * @File : ${NAME}
+ * @Software: ${PRODUCT_NAME}
+ * @Time : ${DATE} ${TIME}
+ */
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
